fdba3b31979139f0d12fb00ecd2e9dffd5ebadb9
vamsi <vamsikiran1303@gmail.com>
2021-11-24 12:49:37 +0530

    working with excell files

--- XLutils.py
+++ XLutils.py
@@ -1,1 +1,23 @@
-print("vamsi")
+
+import openpyxl
+
+def numrows(file,sheetname):
+    workbook=openpyxl.load_workbook(file)
+    sheet=workbook.get_sheet_by_name(sheetname)
+    return sheet.max_row
+
+def nuumcol(file,sheetname):
+    wokbook=openpyxl.load_workbook(file)
+    sheet=wokbook.get_sheet_by_name(sheetname)
+    return sheet.max_column
+
+def getdata(file,sheetname,row,col):
+    wokbook = openpyxl.load_workbook(file)
+    sheet = wokbook.get_sheet_by_name(sheetname)
+    return sheet.cell(row,col).value
+
+def editdata(file,sheetname,row,col,valu):
+    workbook=openpyxl.load_workbook(file)
+    sheet=workbook.get_sheet_by_name(sheetname)
+    sheet.cell(row,col).value=valu
+    workbook.save(file)
--- datadriven.py
+++ datadriven.py
@@ -1,1 +1,13 @@
-print("vamsi")
+import XLutils
+
+path="F:/pycharm practice/nopcommerceApp/database/Book1.xlsx"
+
+print(XLutils.numrows(path,"vamsi"))
+
+print(XLutils.nuumcol(path,"vamsi"))
+
+
+print(XLutils.getdata(path,"vamsi",2,2))
+
+print(XLutils.editdata(path,"vamsi",2,2, "WORLD"))
+print(XLutils.getdata(path,"vamsi",2,2))
