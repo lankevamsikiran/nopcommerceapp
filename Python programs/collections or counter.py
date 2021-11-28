3f7df595f35db591e78f8cbb04f7c5ffc521d4aa
vamsi <vamsikiran1303@gmail.com>
2021-11-28 17:50:50 +0530

    adding file

--- Python programs/collections or counter.py
+++ Python programs/collections or counter.py
@@ -6,7 +6,17 @@ st="abcabcbac adgzxv sdzvx srfzbc "
 a=["vamsi", "kiran","vamsi"]
 b={"vamsi", "kiran","vamsi"}
 c=("vamsi", "kiran","vamsi")
+d= {'a': 4, 'b': 4, 'c': 4, ' ': 4, 'z': 3, 'd': 2, 'x': 2, 'v': 2, 's': 2, 'g': 1, 'r': 1, 'f': 1}
+
 print(Counter(st))
 print(Counter(a))
 print(Counter(b))
 print(Counter(c))
+print(Counter(d))
+
+#NOTE :
+"""
+COUNTER will give output as a dictionary.
+we can use SET also but it will give value as 1 for all keys.
+we can use dictionary also but it will give the same as output"""
+
