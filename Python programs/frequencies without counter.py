e6528e4562a5a724df1da2db77a1e8e6e097d65e
vamsi <vamsikiran1303@gmail.com>
2022-01-08 15:34:36 +0530

    adding files

--- Python programs/frequencies without counter.py
+++ Python programs/frequencies without counter.py
@@ -2,12 +2,21 @@
 arr = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5]
 #arr=["vamsi","kiran","vamsi"]
 ar={}
+#METHOD 1:
+# for i in set(arr):
+#     count=0
+#     for j in arr:
+#         if i==j:
+#             count=count+1
+#     ar[i]=count
+#
+# print(ar)
 
-for i in set(arr):
-    count=0
-    for j in arr:
-        if i==j:
-            count=count+1
-    ar[i]=count
-
+#METHOD 2:
+for i in arr:
+    if i in ar:
+        ar[i] += 1
+    else:
+        ar[i]=1
 print(ar)
+
---
+++ Python programs/second largest number in list.py
@@ -0,0 +1,11 @@
+
+a=[1,14,15,13,2,3,4,5,10,1,9,20]
+second_largest=0
+
+for i in range(len(a)):
+    for j in range(i+1,len(a)):
+        if a[i]>a[j]:
+            a[i],a[j]=a[j],a[i]
+        second_largest=a[i]
+
+print(second_largest)
